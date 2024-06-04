456515d55e118dafc92d7b628ec119789d70d4b9
Saly Jabou <jabou.saly@gmail.com>
2024-06-04 23:20:43 +0200

    added modules

---
+++ src/app/realisations/realisations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-realisations',
+  standalone: true,
+  imports: [],
+  templateUrl: './realisations.component.html',
+  styleUrl: './realisations.component.css'
+})
+export class RealisationsComponent {
+
+}
