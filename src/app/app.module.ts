82351b9c175833e1c783197b520ab5d02c162b42
sjabou <sjabou.externe@m6.fr>
2024-06-12 23:58:43 +0200

    correction bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,61 +1,10 @@
-// // src/app/app.module.ts
-// import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
-// import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
-// import { HomeComponent } from './home/home.component';
-// import { RealisationsComponent } from './realisations/realisations.component';
-// import { ContactComponent } from './contact/contact.component';
-// import { HttpClientModule, HttpClient } from '@angular/common/http';
-// import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-// import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-//
-// export function HttpLoaderFactory(http: HttpClient) {
-//   return new TranslateHttpLoader(http);
-// }
-//
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     HomeComponent,
-//     RealisationsComponent,
-//     ContactComponent
-//   ],
-//   imports: [
-//     BrowserModule,
-//     AppRoutingModule,
-//     HttpClientModule,
-//     TranslateModule.forRoot({
-//       loader: {
-//         provide: TranslateLoader,
-//         useFactory: HttpLoaderFactory,
-//         deps: [HttpClient]
-//       }
-//     })
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
-
-
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { RealisationsComponent } from './realisations/realisations.component';
-import { ContactComponent } from './contact/contact.component';
-
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
-// AoT requires an exported function for factories
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
-}
+import {HomeComponent} from "./home/home.component";
+import {RealisationsComponent} from "./realisations/realisations.component";
+import {ContactComponent} from "./contact/contact.component";
 
 @NgModule({
   declarations: [
@@ -65,21 +14,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     ContactComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule,
-    RouterModule.forRoot([
-      { path: 'home', component: HomeComponent },
-      { path: 'realisations', component: RealisationsComponent },
-      { path: 'contact', component: ContactComponent },
-      { path: '', redirectTo: '/home', pathMatch: 'full' }
-    ]),
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      }
-    })
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
