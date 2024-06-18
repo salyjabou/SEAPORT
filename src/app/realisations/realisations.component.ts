a64240d7ae565e0e3f5bef50a06724e024612db2
sjabou <sjabou.externe@m6.fr>
2024-06-19 00:05:52 +0200

    Service component

--- src/app/realisations/realisations.component.ts
+++ src/app/realisations/realisations.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-realisations',
   templateUrl: './realisations.component.html',
-  styleUrl: './realisations.component.css'
+  styleUrl: './realisations.component.scss'
 })
 export class RealisationsComponent {
 
